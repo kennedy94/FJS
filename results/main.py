bbfb4f3af85e1fca85a7a1e7a619076be4a07fec
Kennedy Araújo <kennedyanderson94@gmail.com>
2025-04-01 10:19:35 -0300

    Results

--- results/main.py
+++ results/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def process_csv(input_file, output_file):
+def process_csv(input_file, instance_group, method):
     data = pd.read_csv(input_file, header=None)
     data = data.iloc[:, -5:]  # Select the last 5 columns
     data.columns = ['instance', 'alfa', 'obj', 'timlim', 'time']
@@ -9,30 +9,38 @@ def process_csv(input_file, output_file):
     data_cv['cv'] = data_cv['std'] / data_cv['mean']
     data_cv['instance_prefix'] = data_cv['instance'].astype(str).str[:2]
 
-    data_cv.groupby(['instance_prefix', 'alfa'], as_index=False)[['cv', 'min']].mean().to_csv(output_file, index=False)
+    data_cv.groupby(['instance_prefix', 'alfa'], as_index=False)[['cv', 'min']].mean() #.to_csv(output_file, index=False)
+    data_cv['method'] = method
+    data_cv['instance_group'] = instance_group
+    data_cv = data_cv.fillna(0)
+    return data_cv
 
-# Example usage
-process_csv('Projetos/FJS/complete-ILS-CN-NewRLS.csv', 'summary-complete-ILS-CN.csv')
-process_csv('Projetos/FJS/complete-ILS-RN-NewRLS.csv', 'summary-complete-ILS-RN.csv')
-process_csv('Projetos/FJS/complete-TS-CN-NewLS.csv', 'summary-complete-TS-CN.csv')
-process_csv('Projetos/FJS/complete-TS-RN-NewLS.csv', 'summary-complete-TS-RN.csv')
-process_csv('Projetos/FJS/complete-grasp-CN-definitivo.csv', 'summary-complete-grasp-CN.csv')
-process_csv('Projetos/FJS/complete-grasp-RN-definitivo.csv', 'summary-complete-grasp-RN.csv')
-process_csv('Projetos/FJS/medium-SA-fix.csv', 'summary-complete-SA.csv')
+# Lista de arquivos e parâmetros
+files = [
+    ('Projetos/FJS/complete-ILS-CN-NewRLS.csv', 'complete', 'ILS-CN'),
+    ('Projetos/FJS/complete-ILS-RN-NewRLS.csv', 'complete', 'ILS-RN'),
+    ('Projetos/FJS/medium-TS-CN-NewLS.csv', 'complete', 'TS-CN'),
+    ('Projetos/FJS/medium-TS-RN-NewLS.csv', 'complete', 'TS-RN'),
+    ('Projetos/FJS/complete-grasp-CN-definitivo.csv', 'complete', 'grasp-CN'),
+    ('Projetos/FJS/complete-grasp-RN-definitivo.csv', 'complete', 'grasp-RN'),
+    ('Projetos/FJS/medium-SA-fix.csv', 'complete', 'SA'),
+    ('Projetos/FJS/mini-ILS-CN-definitivo.csv', 'mini', 'ILS-CN'),
+    ('Projetos/FJS/mini-ILS-RN-definitivo.csv', 'mini', 'ILS-RN'),
+    ('Projetos/FJS/mini-TS-CN-definitivo.csv', 'mini', 'TS-CN'),
+    ('Projetos/FJS/mini-TS-RN-definitivo.csv', 'mini', 'TS-RN'),
+    ('Projetos/FJS/mini-grasp-CN-definitivo.csv', 'mini', 'grasp-CN'),
+    ('Projetos/FJS/mini-grasp-RN-definitivo.csv', 'mini', 'grasp-RN'),
+    ('Projetos/FJS/mini-SA-definitivo.csv', 'mini', 'SA'),
+    ('Projetos/FJS/classic-ILS-CN-definitivo.csv', 'classic', 'ILS-CN'),
+    ('Projetos/FJS/classic-ILS-RN-definitivo.csv', 'classic', 'ILS-RN'),
+    ('Projetos/FJS/classic-TS-CN-definitivo.csv', 'classic', 'TS-CN'),
+    ('Projetos/FJS/classic-TS-RN-definitivo.csv', 'classic', 'TS-RN'),
+    ('Projetos/FJS/classic-grasp-CN-definitivo.csv', 'classic', 'grasp-CN'),
+    ('Projetos/FJS/classic-grasp-RN-definitivo.csv', 'classic', 'grasp-RN'),
+    ('Projetos/FJS/classic-SA-definitivo.csv', 'classic', 'SA'),
+    ('Projetos/FJS/classic-Tayebi.csv', 'classic', 'Tayebi')
+]
 
-process_csv('Projetos/FJS/mini-ILS-CN-definitivo.csv', 'summary-mini-ILS-CN.csv')
-process_csv('Projetos/FJS/mini-ILS-RN-definitivo.csv', 'summary-mini-ILS-RN.csv')
-process_csv('Projetos/FJS/mini-TS-CN-definitivo.csv', 'summary-mini-TS-CN.csv')
-process_csv('Projetos/FJS/mini-TS-RN-definitivo.csv', 'summary-mini-TS-RN.csv')
-process_csv('Projetos/FJS/mini-grasp-CN-definitivo.csv', 'summary-mini-grasp-CN.csv')
-process_csv('Projetos/FJS/mini-grasp-RN-definitivo.csv', 'summary-mini-grasp-RN.csv')
-process_csv('Projetos/FJS/mini-SA-definitivo.csv', 'summary-mini-SA.csv')
-
-process_csv('Projetos/FJS/classic-ILS-CN-definitivo.csv', 'summary-classic-ILS-CN.csv')
-process_csv('Projetos/FJS/classic-ILS-RN-definitivo.csv', 'summary-classic-ILS-RN.csv')
-process_csv('Projetos/FJS/classic-TS-CN-definitivo.csv', 'summary-classic-TS-CN.csv')
-process_csv('Projetos/FJS/classic-TS-RN-definitivo.csv', 'summary-classic-TS-RN.csv')
-process_csv('Projetos/FJS/classic-grasp-CN-definitivo.csv', 'summary-classic-grasp-CN.csv')
-process_csv('Projetos/FJS/classic-grasp-RN-definitivo.csv', 'summary-classic-grasp-RN.csv')
-process_csv('Projetos/FJS/classic-SA-definitivo.csv', 'summary-classic-SA.csv')
-process_csv('Projetos/FJS/classic-Tayebi.csv', 'summary-classic-Tayebi.csv')
+# Concatenando os DataFrames
+df_combined = pd.concat([process_csv(f, c, m) for f, c, m in files], ignore_index=True)
+df_combined.to_csv("results.csv", index=False)
